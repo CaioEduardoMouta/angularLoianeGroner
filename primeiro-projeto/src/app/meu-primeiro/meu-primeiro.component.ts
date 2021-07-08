76a78a830ce7c43210a138014e20b0331ab409b3
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-07-08 03:39:59 -0300

    Angular

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,13 @@
+import { literal } from "@angular/compiler/src/output/output_ast";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com Angular</p>
+    `
+})
+
+export class MeuPrimeiroComponent {
+
+}
